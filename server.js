dad80209e7475c5c0ab6bd2cc291f470be8d6873
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2019-11-16 18:35:55 -0300

    usinf cors

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+// iniciando o app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hello matheus");
-});
+// iniciando o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir("./src/models"); // required do model criado
+
+const Product = mongoose.model('Product');
+
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = express.Router();
+const ProductController = require('../controllers/ProductController');
+// rotas
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+module.exports = routes;
